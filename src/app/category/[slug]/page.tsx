29bd5bad2d69f64404bb5d9a938c6c7e20712dfd
phat-nguyen-alephon-com <phat.nguyen@alephon.com>
2025-02-19 08:59:54 +0700

    [dev] fix params when using nextjs 15

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,22 +1,28 @@
 import FeaturedProducts from '@/components/product/listProduct';
+import {Metadata} from "next";
+type Props = {
+    params: Promise<{ slug: string }>;
+};
 
-export default function CategoryPage({ params }: { params: { slug: string } }) {
-  return (
+export default async function CategoryPage({ params }: Props) {
+    const { slug } = await params;
+
+    return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <h1 className="text-3xl font-bold mb-6 capitalize">
-        {params?.slug?.replace('-', ' ')} Products
+        {slug?.replace('-', ' ')} Products
       </h1>
         <FeaturedProducts />
     </div>
   );
 }
-export async function generateMetadata({
-                                         params
-                                       }: {
-  params: { slug: string }
-}) {
-  return {
-    title: `${params?.slug.replace('-', ' ')} Products`,
-    description: `Browse our ${params?.slug.replace('-', ' ')} collection`,
-  };
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { slug } = await params;
+    const formattedSlug = slug.replace('-', ' ');
+
+    return {
+        title: `${formattedSlug} Products`,
+        description: `Browse our ${formattedSlug} collection`,
+    };
 }
+
